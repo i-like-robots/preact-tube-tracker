7be2571e4aa7da87596acdff76c763d0911ef032
Matt Hinchliffe <matt@maketea.co.uk>
2016-03-26 23:06:49 +0000

    Switch to use separate Node and Webpack presets

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-require('babel-core/register')
+require('babel-core/register')({
+  presets: ['es2015-node5'],
+  plugins: [
+    [ 'transform-react-jsx', { 'pragma': 'h' } ]
+  ]
+})
 require('./src/server/server')
